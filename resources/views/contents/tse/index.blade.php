338b67d7ecb9cee9402501b3e8f5240437e0f0b8
lolzxc <guitarbeginner2@gmail.com>
2022-08-03 08:50:12 +0800

    Fixed TSE index ui

--- resources/views/contents/tse/index.blade.php
+++ resources/views/contents/tse/index.blade.php
@@ -2,10 +2,6 @@
 
 @section('stylesheet')
 <style>
-    html,
-    body {
-        background-color: #00A1FC;
-    }
 </style>
 @endsection
 @section('content')
@@ -34,21 +30,21 @@
 <div class="content text-white d-flex flex-grow-1 flex-column h-100">
     <div class="container">
         <div class="header d-flex justify-content-center align-items-center pt-5 border-bottom border-white">
-            <span class="fw-bold fs-1 mb-1">Feedbacks Issued</span>
+            <span class="fw-bold text-center title-main mb-1">Feedbacks Issued</span>
         </div>
 
     </div>
 
     <div class="list-group">
         <div class="container mt-1">
-            @foreach (array_reverse($filtered_feedbacks) as $filtered_feedback)
-            <a href="{{ route('view-feedback', [$filtered_feedback[0]['user_id'], $filtered_feedback[0]['id']]) }}" class="list-group-item list-group-item-action" aria-current="true">
-                <small class="text-end">{{ $filtered_feedback[0]['updated_at']->format('F j Y, g:i a') }}</small>
-                <h5 class="mb-1">{{ $filtered_feedback[0]['message'] }}</h5>
-                <small class="border rounded-pill p-1 border-primary d-inline-block">{{ $filtered_feedback[0]['system'] }}</small>
-                <small class="border rounded-pill p-1 border-primary d-inline-block">{{ $filtered_feedback[0]['module'] }}</small>
+            @foreach ($filtered_feedbacks as $filtered_feedback)
+            <a href="{{ route('view-feedback', [$filtered_feedback->user_id, $filtered_feedback->id]) }}" class="list-group-item list-group-item-action" aria-current="true">
+                <small class="text-end fw-bold">{{ date('F j Y, g:i a', strtotime($filtered_feedback->updated_at)) }}</small>
+                <h5 class="mb-1">{{ $filtered_feedback->message }}</h5>
+                <small class="border rounded-pill p-1 border-primary d-inline-block">{{ $filtered_feedback->system }}</small>
+                <small class="border rounded-pill p-1 border-primary d-inline-block">{{ $filtered_feedback->module }}</small>
                 <br>
-                <small class="border rounded-pill mt-1 p-1 text-center d-inline-block text-white fs-5 fw-bold" style="background-color: brown">{{ $filtered_feedback[0]['status'] }}</small>
+                <small class="border rounded-pill mt-1 p-1 text-center d-inline-block text-white fs-5 fw-bolder" style="background-color: brown">{{ $filtered_feedback->status }}</small>
             </a>
             @endforeach
         </div>
